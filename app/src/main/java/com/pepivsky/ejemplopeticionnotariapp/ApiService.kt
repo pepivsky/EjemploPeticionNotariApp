90ae856c0ddc46cbc638ad9009fa450d9361402a
pepivsky <ironglas68@gmail.com>
2021-04-29 14:59:26 -0500

    Peticion funcional

--- app/src/main/java/com/pepivsky/ejemplopeticionnotariapp/ApiService.kt
+++ app/src/main/java/com/pepivsky/ejemplopeticionnotariapp/ApiService.kt
@@ -1,25 +1,27 @@
 package com.pepivsky.ejemplopeticionnotariapp
 
 import com.pepivsky.ejemplopeticionnotariapp.response.Respuesta
-import okhttp3.Call
-import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import okhttp3.Response
-import retrofit2.http.*
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import retrofit2.http.Part
+
 
 interface ApiService {
-
-    @Multipart
+/*
+   // @Multipart
     @POST("users/signup")
     //@FormUrlEncoded
     fun registerUser(
-        @Part("person_type") personType: RequestBody,
+        @Part() personType: RequestBody,
         @Part("firstname") firstName: RequestBody,
         @Part("middlename") middleName: RequestBody,
         @Part("lastname") lasName: RequestBody,
         @Part("secondlastname") secondLastName: RequestBody,
         @Part("type_doc") typeDoc: RequestBody,
-        @Part("num_doc") numDoc: RequestBody,
+        @Part("num\"person_type\"_doc") numDoc: RequestBody,
         @Part("birthday") birthDay: RequestBody,
         @Part("nit") nit: RequestBody,
         @Part("company_name") companyName: RequestBody,
@@ -31,22 +33,32 @@ interface ApiService {
         @Part("confirm_email") ConfirmEmail: RequestBody,
         @Part("password") password: RequestBody,
         @Part("confirm_password") confirmPassword: RequestBody
-    )/*: Call<Respuesta>*/
+    ) Call<Respuesta>*/
 }
 
-/*
-    @Multipart
-    @POST
-    @FormUrlEncoded
-    fun registerUser(@Field part: MultipartBody,
-                     @Field("person_type") personType: Int
-    )
-     */
+interface NotariService {
 
-/*@Multipart
-@POST("http://3.219.19.170:3000/users/signup")
-Call<ResponseBody> registerUser(@Part MultipartBody.Part image,
-@Part("email") RequestBody email,
-@Part("password") RequestBody password,
-@Part("nombre") RequestBody nombre
-);*/
+    @POST("users/signup") //metodo post con su url
+    @FormUrlEncoded //post de tipo:  x-www-form-urlencoded
+    fun createUser(
+        @Field("person_type") personType: String,
+        @Field("firstname") firstName: String,
+        @Field("middlename") middleName: String,
+        @Field("lastname") lastName: String,
+        @Field("secondlastname") secondLastName: String,
+        @Field("type_doc") typeDoc: String,
+        @Field("num_doc") numDoc: String,
+        @Field("birthday") birthDay: String,
+        @Field("nit") nit: String,
+        @Field("company_name") companyName: String,
+        @Field("phone") phone: String,
+        @Field("dpto") dpto: String,
+        @Field("city") city: String,
+        @Field("address") address: String,
+        @Field("email") email: String,
+        @Field("confirm_email") confirmEmail: String,
+        @Field("password") password: String,
+        @Field("confirm_password") confirmPassword: String
+
+        ): Call<Respuesta> //devuelve un objeto Respuesta
+}
--- app/src/main/java/com/pepivsky/ejemplopeticionnotariapp/MainActivity.kt
+++ app/src/main/java/com/pepivsky/ejemplopeticionnotariapp/MainActivity.kt
@@ -6,9 +6,10 @@ import android.util.Log
 import com.pepivsky.ejemplopeticionnotariapp.response.Respuesta
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
 import okhttp3.RequestBody
-import retrofit2.Callback
-import retrofit2.Retrofit
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.log
 
@@ -17,69 +18,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val TYPE_MULTIPART: MediaType? = "multipart/form-data".toMediaTypeOrNull()
+        val baseURL = "http://3.219.19.170:3000/"
 
-        val call = getRetrofit().create(ApiService::class.java)
-            .registerUser(
-                RequestBody.create(TYPE_MULTIPART, "1"),
-                RequestBody.create(TYPE_MULTIPART, "josepruebapepe"),
-                RequestBody.create(TYPE_MULTIPART, "manuel"),
-                RequestBody.create(TYPE_MULTIPART, "paredes"),
-                RequestBody.create(TYPE_MULTIPART, "ramirez"),
-                RequestBody.create(TYPE_MULTIPART, "1"),
-                RequestBody.create(TYPE_MULTIPART, "34534534345"),
-                RequestBody.create(TYPE_MULTIPART, "1980-01-05"),
-                RequestBody.create(TYPE_MULTIPART, "9004585222"),
-                RequestBody.create(TYPE_MULTIPART, "sometido Itda"),
-                RequestBody.create(TYPE_MULTIPART, "3015648975"),
-                RequestBody.create(TYPE_MULTIPART, "casanare"),
-                RequestBody.create(TYPE_MULTIPART, "pradera"),
-                RequestBody.create(TYPE_MULTIPART, "Av siempre viva"),
-                RequestBody.create(TYPE_MULTIPART, "marcos2@gmail.com"),
-                RequestBody.create(TYPE_MULTIPART, "marcos2@gmail.com"),
-                RequestBody.create(TYPE_MULTIPART, "automan123Q."),
-                RequestBody.create(TYPE_MULTIPART, "automan123Q.")
-            )
+        //objeto logger
+        val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
+        //HTTP client
+        val client = OkHttpClient.Builder().addInterceptor(logger)
 
-
-
-
-        // Log.i("usuarioGuardado", response.toString())
-
-        //Log.i("guardado", "${call.toString()} $call")
-
-
-    }
-
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("http://3.219.19.170:3000/")
+        //objeto builder
+        val builder = Retrofit.Builder().baseUrl(baseURL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        //agregando el cliente http
+            .client(client.build())
+
+        //objeto retrofit
+        val retrofit = builder.build()
+
+        //haciendo la peticion
+        Log.i("onCreate", "Haciendo la peticion")
+        val call = retrofit.create(NotariService::class.java) //pasandole la interfaz que contiene los endpoints
+        //llamando al metodo y pasandole la informacion
+        call.createUser(
+            "1",
+            "Shiro",
+            "manuel",
+            "paredes",
+            "ramirez",
+            "1",
+            "2131232",
+            "1997-03-05",
+            "213123131",
+            "algo",
+            "7731295070",
+            "sdlkajsdlkajkdl",
+            "barranquilla",
+            "vivo en alguna parte",
+            "pepe@gmail.com",
+            "pepe@gmail.com",
+            "pepe1997A_",
+            "pepe1997A_"
+        ).enqueue(object : Callback<Respuesta>{
+            override fun onResponse(call: Call<Respuesta>, response: Response<Respuesta>) {
+                if (response.isSuccessful) {
+                    Log.i("Exito! : ", "${response.body()}")
+                } else {
+                    Log.i("else", ":(")
+                }
+            }
+
+            override fun onFailure(call: Call<Respuesta>, t: Throwable) {
+                Log.i("bad", "Algo salio mal")
+            }
+
+        })
     }
-
-
-    /*
-    "users/signup",
-                null,
-                1,
-                "jose",
-                "manuel",
-                "paredes",
-                "ramirez",
-                1,
-                "34534534345",
-                "1980-01-05",
-                "9004585222",
-                "sometido Itda",
-                "3015648975",
-                "casanare",
-                "pradera",
-                "Av siempre viva",
-                "marcos2@gmail.com",
-                "marcos2@gmail.com",
-                "automan123Q.",
-                "automan123Q.")
-     */
 }
